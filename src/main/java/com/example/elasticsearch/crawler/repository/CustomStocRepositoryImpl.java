43d4b262975b3de57a69ba1b2247f0ffeb715f4e
tjdwns4537 <tjdwns4537@naver.com>
2023-04-12 23:44:55 +0900

    feat: QueryDSL bean 등록 및 findByName 함수 작성

--- src/main/java/com/example/elasticsearch/crawler/repository/CustomStocRepositoryImpl.java
+++ src/main/java/com/example/elasticsearch/crawler/repository/CustomStocRepositoryImpl.java
@@ -1,30 +1,31 @@
 package com.example.elasticsearch.crawler.repository;
 
 import com.example.elasticsearch.stock.domain.QStockDbDto;
-import com.example.elasticsearch.stock.domain.Stock;
 import com.example.elasticsearch.stock.domain.StockDbDto;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import java.util.List;
-
+@RequiredArgsConstructor
 @Repository
 @Transactional
 public class CustomStocRepositoryImpl implements CustomStockRepository {
 
     @Autowired
-    EntityManager em;
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public StockDbDto findByStockName(String stockName) {
-        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
         QStockDbDto qStockDbDto = QStockDbDto.stockDbDto;
 
-        StockDbDto stockDbDto = queryFactory.select(qStockDbDto)
+        StockDbDto resultStock = jpaQueryFactory.select(qStockDbDto)
                 .from(qStockDbDto)
                 .where(qStockDbDto.stockName.eq(stockName))
-                .fetchOne();    }
+                .fetchOne();
+
+        return resultStock;
+    }
 }
